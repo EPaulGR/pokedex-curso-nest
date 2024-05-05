1770a379c83790d0f593e8ec388496dfd3e45415
Paul <epaulgrmx@gmail.com>
2024-05-04 20:06:48 -0600

    esta listo borrar

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -64,9 +64,11 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    const pokemon = await this.findOne(id);
-    await pokemon.deleteOne();
-    return `This pokemon was deleted #${id}`;
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
+    if (deletedCount === 0) {
+      throw new BadRequestException(`Pokemon not found with id_ ${id}`);
+    }
+    return `This action removes a #${id} pokemon`;
   }
 
   // Functions
