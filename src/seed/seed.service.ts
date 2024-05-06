59ac8e4b889aa3ee76581c4a5e8069c71359e02d
Paul <epaulgrmx@gmail.com>
2024-05-05 19:10:22 -0600

    lista la paginación

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,24 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
-
   constructor(
     @InjectModel(Pokemon.name)
-    private pokemonModel: Model<Pokemon>,
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
     // borro lo que existe
     await this.pokemonModel.deleteMany({});
     // obtengo los nuevos datos
-    const { data } = await axios.get<PokeResponse>(
+    const data = await this.http.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
     // construyo el nuevo array
